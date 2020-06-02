1dae5030dac044914f12e05f868211888794e9d7
lumat <lumat@op.pl>
2020-06-02 21:51:41 +0200

    Initial commit

---
+++ src/main/java/pl/bykowski/websocketchat/Message.java
@@ -0,0 +1,12 @@
+package pl.bykowski.websocketchat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String value;
+}
